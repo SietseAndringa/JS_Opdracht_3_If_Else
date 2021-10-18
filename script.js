8bf95ff514065bef2167feab42433f3a97b27a28
SietseAndringa <82322340+SietseAndringa@users.noreply.github.com>
2021-10-18 18:06:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+*/
+
+const age = 15; 
+const isFemale = false;
+const driverStatus = "bert"; 
+
+
+if (age >= 18) {
+  console.log("Je bent minimaal 18 jaar, dus mag je naar binnen!");
+} else {
+  console.log("Je bent nog geen 18 jaar. Je mag niet naar binnen!");
+}
+
+if (isFemale) {
+  console.log("Je bent een vrouw. Gezellige avond!");
+} else {
+  console.log("Je bent een man. Helaas mag je dan niet naar binnen!");
+}
+
+if (driverStatus == "bob") {
+  console.log("Je bent de bob. Rij voorzichtig!");
+} else {
+  console.log("Je bent geen bob. Laat iemand anders rijden!");
+}
